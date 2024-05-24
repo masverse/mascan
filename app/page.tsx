8e2fb7ae9bf297d806fe5b75542345f5a0b85289
jason.aw <jason.ahw@masverse.com.my>
2024-05-24 16:46:58 +0800

    remove testnet

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import Search from "@/components/Search";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center relative">
       <Search />
-      <Link href="/testnet" className="bg-slate-400 rounded-full px-3 py-2 font-bold text-white mt-4">
-        TESTNET
-      </Link>
     </main>
   );
 }
