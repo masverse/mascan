f831792f90f76aa2a2dde31fb5740fd05ba5faa5
jason.aw <jason.ahw@masverse.com.my>
2024-05-31 00:39:36 +0800

    change on button

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,18 +1,19 @@
 'use client'
 import { useState } from "react";
 import Link from "next/link";
+import { Button } from "./ui/moving-border";
 
 const Search = () => {
 
     const [txid, settxid] = useState("")
 
     return (
-            <div className="bg-white w-2/4 h-16 rounded-xl px-5 flex items-center mt-16 relative z-10">
-                <input className="w-full outline-none text-black" type="text" placeholder="Transaction ID" onChange={(e) => settxid(e.target.value)}/>
-                <Link href={`/${txid}`}>
-                    <button className="bg-gray-800 outline-none text-white rounded-lg px-7 py-2">Search</button>
-                </Link>
-            </div>
+        <div className="bg-white md:w-3/4 w-5/6 h-16 rounded-xl px-5 flex items-center mt-16 relative z-10">
+            <input className="w-full outline-none text-black" type="text" placeholder="Transaction ID" onChange={(e) => settxid(e.target.value)} />
+            <Link href={`/${txid}`}>
+                <Button className="bg-gray-800 outline-none text-white rounded-lg px-7 py-2">Search</Button>
+            </Link>
+        </div>
     )
 }
 
